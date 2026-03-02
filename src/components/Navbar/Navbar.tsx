b04007b22f1a34586339a9f76dd073118aa54db3
leolopezOsornio <leol83377@gmail.com>
2026-03-02 17:22:05 -0600

    style(Navbar, index): Estilo de el componente nabvar y Site Identity.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,17 @@
 import { NavLink } from 'react-router-dom'
 import styles from './Navbar.module.css'
+import logo from '../../assets/logo/LOGOS-01.png'
 
 export function Navbar() {
   return (
     <header className={styles.navbar}>
       <div className={styles.container}>
-        <div className={styles.logo}>JOLI</div>
-
+        <div className={styles.logo}>
+          <NavLink to="/">
+            <img src={logo} alt="JOLI logo" />
+          </NavLink>
+        </div>
         <nav className={styles.links}>
-          <NavLink to="/">Inicio</NavLink>
           <NavLink to="/nosotros">Nosotros</NavLink>
           <a href="#servicios">Servicios</a>
           <a href="#contacto">Contacto</a>
