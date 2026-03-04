53497e557ef2276b71dbc5295d7bb0a7b672f87a
leolopezOsornio <leol83377@gmail.com>
2026-03-04 17:25:38 -0600

    feat(Home/nabar): Creacion del banner, arreglo de la navbar.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,10 +3,11 @@ import styles from './Home.module.css'
 export function Home() {
     return (
         <section className={styles.home}>
-            <div className={styles.container}>
-                <div className={styles.hero}>
+
+            <div className={styles.banner}>
+                <div className={styles.heroBanner}>
                     <h1 className={styles.title}>
-                        Bienvenido al portal JoliWeb
+                        Bienvenido al portal <span className={styles.highlight}>JoliWeb</span>
                     </h1>
 
                     <p className={styles.subtitle}>
@@ -21,6 +22,22 @@ export function Home() {
                         <button className="btn btn-secondary">
                             Saber más
                         </button>
+                    </div>
+                </div>
+            </div>
+
+            <div className={styles.container}>
+                <div className={styles.hero}>
+                    <h1 className={styles.title}>
+                        Bienvenido al portal JoliWeb
+                    </h1>
+
+                    <h3 className={styles.subtitle}>
+                        Un lugar donde la web es más bonita y funcional.
+                    </h3>
+
+                    <div className={styles.bodyText}>
+                        <p>Hola nosotros somo joli, una financiadora de seguros donde puedes haceer crecer tu pension hasta un 300% más!!!</p>
                     </div>
                 </div>
             </div>
