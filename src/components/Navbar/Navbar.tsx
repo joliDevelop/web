6b59b6b55ad48f2e1c8432f9b63b88323ff3ed69
leolopezOsornio <leol83377@gmail.com>
2026-03-18 11:10:04 -0600

    style(navbar): Mejora de estilo.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,35 @@
 import { NavLink } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import styles from './Navbar.module.css'
 import logo from '../../assets/logo/LOGOS-01.png'
+import logoWhite from '../../assets/logo/LOGOS-03.png'
 
 export function Navbar() {
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 80)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
-    <header className={styles.navbar}>
+    <header className={`${styles.navbar} ${scrolled ? styles.scrolled : ''}`}>
       <div className={styles.container}>
         <div className={styles.logo}>
           <NavLink to="/">
-            <img src={logo} alt="JOLI logo" />
+            <img src={scrolled ? logoWhite : logo} alt="JOLI logo" />
           </NavLink>
         </div>
+
         <nav className={styles.links}>
           <NavLink to="/nosotros">Nosotros</NavLink>
           <a href="#servicios">Servicios</a>
           <a href="#contacto">Contacto</a>
         </nav>
-
       </div>
     </header>
   )
