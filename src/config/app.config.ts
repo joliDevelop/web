58e6c7d873f1fbf155f247eecd1b96eda1ffbd0a
Hector Cervantes <cervaya.003@gmail.com>
2026-03-18 15:18:39 -0600

    refactor(servise, config, maps): variables globales 2

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,4 +1,4 @@
 export const CONFIG = {
-  API_URL: 'https://joli-dev-732206415367.europe-west1.run.app/api',
+  API_URL: 'https://joli-dev-732206415367.northamerica-south1.run.app/api',
   MAPS_API_KEY: 'AIzaSyARlQRYytiRJvKOKqrb1uUgWrRAmLTyak4',
 }
