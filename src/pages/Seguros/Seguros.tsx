8a5b0ebbd869929ddf07a22f37481d54b6adf032
leolopezOsornio <leol83377@gmail.com>
2026-03-27 12:02:35 -0600

    feat(Seguros): Inicialización de la pagina.

--- src/pages/Seguros/Seguros.tsx
+++ src/pages/Seguros/Seguros.tsx
@@ -1,10 +1,31 @@
 import styles from './Seguros.module.css'
+import { Banner } from '../../components/shared/Banner/Banner'
+import bannerSeguros from '../../assets/images/banner1.webp'
+import { SectionSubtitle } from '../../components/shared/SectionSubtitle/SectionSubtitle'
+export function Seguros() {
 
-export function Seguros () {
-
-    return(
-        <>
-        Hola mundo jsjsj
-        </>
+    return (
+        <section className={styles.seguros}>
+            <Banner
+                backgroundImage={bannerSeguros}
+                title={
+                    <>
+                        PROTECCIÓN <span className={styles.highlight}>INTELIGENTE
+                            PARA LO QUE</span> MÁS VALORAS.
+                    </>
+                }
+                subtitle=""
+                buttonText=""
+            />
+            <div className={styles.container}>
+                <div className={styles.hero}>
+                    <section className={styles.containerTittle}>
+                        <SectionSubtitle>
+                            <span className={styles.highlight2}>TIPOS DE </span>COBERTURA
+                        </SectionSubtitle>
+                    </section>
+                </div>
+            </div>
+        </section>
     )
 }
